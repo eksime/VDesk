921175e8b59f74a05093422b21a431e6a3e0d4d6
Elliot Sime <e.sime@lancs.ac.uk>
2016-01-22 11:57:17 +0000

    Partial rewrite, cleaned up code.

--- VDesk/Program.cs
+++ VDesk/Program.cs
@@ -1,49 +1,66 @@
 ﻿using System;
 using libVDesk;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.ComponentModel;
 
 namespace VDesk {
   static class Program {
+    static readonly int desktopIndex = 0;
+    static readonly int executable = 1;
+    static readonly int arguments = 2;
+
     static void Main(string[] args) {
       if (Environment.OSVersion.Version.Major != 10) return; //run only on Windows 10
       if (args.Length == 0) return; //need at least 1 arg.
 
-      //remove own executable name from command line
-      String commandLineArgs = Environment.CommandLine.Remove(0, Environment.CommandLine.IndexOf(" ") + 2);
-
+      String[] clArgs = parseArgs(Environment.CommandLine);
       Process proc = new Process();
-      Int32 index = -1; //index of desktop to use. Vlaues < 0 create a new desktop.
-      Int32 param = 0; 
 
-      if (int.TryParse(args[param], out index)) {
-        commandLineArgs = commandLineArgs.Remove(0, args[0].Length + 1); //remove index from command line
-        index--; //use human numbering for desktops {1 ...} rather than {0 ...}
-        param = 1; //second param is executable name
-      }
+      int index = int.Parse(clArgs[desktopIndex]) - 1; //set desktop index
+      proc.StartInfo.FileName = clArgs[executable]; //set executable name
+      proc.StartInfo.Arguments = clArgs[arguments]; //set arg list
 
-      proc.StartInfo.FileName = args[param]; //set executable name
-
-      if (args.Length > param + 1) { //remove second arg & pass all other args through to second program.
-        proc.StartInfo.Arguments = commandLineArgs.Remove(0, args[param].Length + (commandLineArgs.StartsWith("\"") ? 3 : 1));
-      }
-      
       //If we're opening a program on desktop 10, ensure there are 10 desktops.
-      for (int i = Desktop.Count - 1; i < index; i++) {
+      for (int i = Desktop.Count - 1; i < index; i++) 
         Desktop.Create();
-      }
-
-      //get the desktop, or create a new one, switch to it, and start the program.
+      
+      //get the desktop, or create a new one, and switch to it
       Desktop desk = index < 0 ? Desktop.Create() : Desktop.FromIndex(index);
       desk.MakeVisible();
-      proc.Start();
-      
-      //If we created a desktop just for this program, remove it after the program has finished executing.
-      if (index < 0) {
-        proc.WaitForExit();
-        desk.Remove();
+
+      try {
+        //try starting the program
+        proc.Start();
+
+      } catch (Win32Exception) {
+        //Error launching program.
+        Console.Error.WriteLine("Failed to start program.\nCheck executable path.");
+
+      } finally {
+        //If we created a desktop just for this program, remove it after the program has finished executing.
+        if (index < 0) {
+          proc.WaitForExit();
+          desk.Remove();
+        }
+
       }
 
       return;
+    }
+
+    static String[] parseArgs(String cla) {
+      String[] ret = new string[3];
+      cla = cla.Remove(0, cla.IndexOf("  ")).Trim(); //remove executable name
+      GroupCollection groups = Regex.Match(cla, "(?:(-?\\d+) )?(\"[^\"]+\"|[\\w-:_/.\\\\]+) ?(.*)").Groups;
+      
+      for (int i = 1; i < 4; i++)
+        ret[i-1] = groups[i].Value;
+      
+      if (ret[0].Equals(""))
+        ret[0] = "-1";
+
+      return ret;
     }
   }
 }
