3251a8441288e4aaf0b736554a4ec72b49e37c37
Elliot Sime <e.sime@lancs.ac.uk>
2016-01-22 08:25:07 +0000

    Add feature
    VDesk can launch programs on specific desktops using VDesk [n]
    <executable> [args] the parameter is optional.

--- VDesk/Program.cs
+++ VDesk/Program.cs
@@ -7,22 +7,40 @@ namespace VDesk {
     static void Main(string[] args) {
       if (args.Length == 0) return; //need at least 1 arg.
 
-      Process proc = new Process();
-      proc.StartInfo.FileName = args[0];
+      //remove own executable name from command line
+      String commandLineArgs = Environment.CommandLine.Remove(0, Environment.CommandLine.IndexOf(" ") + 2);
 
-      if (args.Length > 1) {
-        String commandLineArgs = Environment.CommandLine.Remove(0, Environment.CommandLine.IndexOf(" ") + 2); //remove own executable name
-        proc.StartInfo.Arguments = commandLineArgs.Remove(0, args[0].Length + (commandLineArgs.StartsWith("\"") ? 3 : 1)); //remove second arg & pass all other args through to second program.
+      Process proc = new Process();
+      Int32 index = -1; //index of desktop to use. Vlaues < 0 create a new desktop.
+      Int32 param = 0; 
+
+      if (int.TryParse(args[param], out index)) {
+        commandLineArgs = commandLineArgs.Remove(0, args[0].Length + 1); //remove index from command line
+        index--; //use human numbering for desktops {1 ...} rather than {0 ...}
+        param = 1; //second param is executable name
       }
 
-      //Create a new desktop and launch the program; wait for the program to close, and remove the desktop.
-      var desk = Desktop.Create(); 
+      proc.StartInfo.FileName = args[param]; //set executable name
+
+      if (args.Length > param + 1) { //remove second arg & pass all other args through to second program.
+        proc.StartInfo.Arguments = commandLineArgs.Remove(0, args[param].Length + (commandLineArgs.StartsWith("\"") ? 3 : 1));
+      }
+      
+      //If we're opening a program on desktop 10, ensure there are 10 desktops.
+      for (int i = Desktop.Count - 1; i < index; i++) {
+        Desktop.Create();
+      }
+
+      //get the desktop, or create a new one, switch to it, and start the program.
+      Desktop desk = index < 0 ? Desktop.Create() : Desktop.FromIndex(index);
       desk.MakeVisible();
-
       proc.Start();
-      proc.WaitForExit();
-
-      desk.Remove();
+      
+      //If we created a desktop just for this program, remove it after the program has finished executing.
+      if (index < 0) {
+        proc.WaitForExit();
+        desk.Remove();
+      }
 
       return;
     }
