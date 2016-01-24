4aab4d043f97e62070b2a6453878fe1fc5b76d36
Elliot Sime <e.sime@lancs.ac.uk>
2016-01-24 01:46:23 +0000

    Add context menu support

--- VDesk/Program.cs
+++ VDesk/Program.cs
@@ -3,28 +3,61 @@ using libVDesk;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
 using System.ComponentModel;
+using Microsoft.Win32;
 
 namespace VDesk {
   static class Program {
-    static readonly int desktopIndex = 0;
-    static readonly int executable = 1;
-    static readonly int arguments = 2;
+    static readonly int executable = 0;
+    static readonly int desktopIndex = 1;
+    static readonly int command = 2;
+    static readonly int arguments = 3;
 
+    
     static void Main(string[] args) {
-      if (Environment.OSVersion.Version.Major != 10) return; //run only on Windows 10
       if (args.Length == 0) return; //need at least 1 arg.
 
-      String[] clArgs = parseArgs(Environment.CommandLine);
+      switch (args[0]) {
+        case "-install": install(); return;  //add registry entries for context menu
+        case "-uninstall": uninstall(); return;  //remove registry entries for context menu
+        default: run(); return;
+      }
+    }
+
+    static void install() {
+      RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Classes\*\shell", true);
+
+      RegistryKey vdesk = key.CreateSubKey("VDesk");
+      vdesk.SetValue("", "Open in new virtual desktop");
+
+      RegistryKey command = vdesk.CreateSubKey("command");
+      command.SetValue("", "\"" + System.Reflection.Assembly.GetEntryAssembly().Location + "\" \"%1\" %*");
+
+      command.Close();
+      vdesk.Close();
+      key.Close();
+    }
+
+    static void uninstall() {
+      RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Classes\*\shell", true);
+
+      if (key.OpenSubKey("VDesk") != null) //check key exists
+        key.DeleteSubKeyTree("VDesk");
+        
+      key.Close();
+    }
+
+    static void run() {
+      String[] clArgs = parseCommandLine(Environment.CommandLine);
       Process proc = new Process();
 
       int index = int.Parse(clArgs[desktopIndex]) - 1; //set desktop index
-      proc.StartInfo.FileName = clArgs[executable]; //set executable name
+      proc.StartInfo.FileName = clArgs[command]; //set executable name
       proc.StartInfo.Arguments = clArgs[arguments]; //set arg list
 
       //If we're opening a program on desktop 10, ensure there are 10 desktops.
-      for (int i = Desktop.Count - 1; i < index; i++) 
+      for (int i = Desktop.Count - 1; i < index; i++)
         Desktop.Create();
-      
+
       //get the desktop, or create a new one
       Desktop desk = index < 0 ? Desktop.Create() : Desktop.FromIndex(index);
 
@@ -50,21 +83,20 @@ namespace VDesk {
         }
       }
 
-      Console.ReadLine();
       return;
     }
 
-    static String[] parseArgs(String cla) {
-      String[] ret = new string[3];
-      cla = cla.Remove(0, cla.IndexOf("  ")).Trim(); //remove executable name
-      GroupCollection groups = Regex.Match(cla, "(?:(-?\\d+) ?)?(\"[^\"]+\"|[\\w-:_\\/.\\\\]+)? ?(.*)").Groups;
+    static String[] parseCommandLine(String cla) {
+      String[] ret = new string[4];
+
+      GroupCollection groups = Regex.Match(cla, @"(\""[^\""]+\""|[\w-:_\/\.\\]+) +(?:(-?\d+) ?)?(\""[^\""]+\""|[\w-:_\/.\\]+)? ?(.*)").Groups;
       
       for (int i = 1; i < 4; i++)
         ret[i-1] = groups[i].Value; //set return values
       
-      if (ret[0].Equals(""))
-        ret[0] = "0";  //if index is empty, set index = 0
-
+      if (ret[desktopIndex].Equals(""))
+        ret[desktopIndex] = "0";  //if index is empty, set index = 0
+      
       return ret;
     }
   }
