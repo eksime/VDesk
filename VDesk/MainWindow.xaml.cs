5b5caf9b9f37ca72d5aadb9b6f96ad59076cda04
Elliot Sime <eksime@gmail.com>
2017-05-03 19:40:18 +0100

    VDesk 1.0

---
+++ VDesk/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows;
+using WindowsDesktop;
+
+namespace VDesk {
+    partial class MainWindow {
+        public ObservableCollection<VirtualDesktop> desktops;
+
+        public MainWindow() {
+            desktops = new ObservableCollection<VirtualDesktop>(VirtualDesktop.GetDesktops());
+            VirtualDesktop.Created += VirtualDesktop_Created;
+            VirtualDesktop.Destroyed += VirtualDesktop_Destroyed;
+            InitializeComponent();
+            view.DataContext = this;
+            lstDesktops.ItemsSource = desktops;
+        }
+
+        private void VirtualDesktop_Destroyed(object sender, VirtualDesktopDestroyEventArgs e) {
+            desktops.Remove(e.Destroyed);
+        }
+
+        private void VirtualDesktop_Created(object sender, VirtualDesktop e) {
+            desktops.Add(e);
+        }
+
+        private void CreateNew(object sender, RoutedEventArgs e) {
+            VirtualDesktop.Create().Switch();
+        }
+        
+    }
+}
--- VDesk/Properties/Resources.Designer.cs
+++ VDesk/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace vdesk.Properties {
+namespace VDesk.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace vdesk.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("vdesk.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VDesk.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
